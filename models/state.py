2b198a1bfec9fc04a7565a410ad068f2b8a00e7c
EMMANUEL AYOBAMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-01-17 23:28:39 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -8,16 +8,20 @@ from models.city import City
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
+
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        from models import storage
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """get a list of all related city instances
+            with state_id = to the current state id
+            """
+            cities_list = []
 
-        city_inst = [inst for inst in storage.all().values()
-                     if isinstance(inst, City)]
-        result = [cities for cities in city_inst if cities.state_id == self.id]
-        return result
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
