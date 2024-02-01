a077ced05d3c022951bb7563a442740b6403bbc1
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-01 06:35:31 -0800

    deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,52 +1,27 @@
 #!/usr/bin/python3
-from fabric import task
+"""
+Fabric script to genereate tgz archive
+
+
+
+
+
+"""
+
 from datetime import datetime
-import os
+from fabric.api import *
 
 
-@task
-def do_pack(c):
-    """Create a .tgz archive from the contents of web_static folder"""
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
-    archive_filename = "web_static_{}.tgz".format(timestamp)
-    archive_folder = "versions"
+def do_pack():
+    """
+    making an archive on web_static folder
+    """
 
-    if not os.path.exists(archive_folder):
-        os.makedirs(archive_folder)
-
-    archive_path = os.path.join(archive_folder, archive_filename)
-    command = "tar -czvf {} web_static".format(archive_path)
-    result = c.local(command)
-
-    if result.failed:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
-
-    return archive_path
-
-
-@task
-def deploy(c):
-    """Deploy the web_static archive"""
-    archive_path = do_pack(c)
-    if not archive_path:
-        print("Archive creation failed.")
-        return
-
-    # Your deployment logic goes here
-    # For example, you can use fabric's put() function to upload the archive to a remote server
-    # and extract it to the desired location on the server
-    # You can also perform any necessary setup or configuration tasks
-
-    print("Deployment completed successfully.")
-
-
-# Entry point for running the task
-def main(c):
-    deploy(c)
-
-
-# Task runner
-if __name__ == "__main__":
-    task_runner = task(main)
-    task_runner()
