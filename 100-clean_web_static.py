533620bf17aa31fe5d62a9aff9b82e574ecf602b
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-01 15:36:59 -0800

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['18.210.20.231', '18.208.119.93']
 
 
 def do_clean(number=0):
